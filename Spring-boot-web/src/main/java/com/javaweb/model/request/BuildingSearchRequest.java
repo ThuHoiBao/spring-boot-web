1e8db06375d8616baaab627b655bf124d5b0a7b9
ThuHoiBao <166045722+ThuHoiBao@users.noreply.github.com>
2024-10-11 22:59:18 +0700

    second

--- Spring-boot-web/src/main/java/com/javaweb/model/request/BuildingSearchRequest.java
+++ Spring-boot-web/src/main/java/com/javaweb/model/request/BuildingSearchRequest.java
@@ -1,5 +1,22 @@
 package com.javaweb.model.request;
 
-public class BuildingSearchRequest {
+import java.util.List;
 
+public class BuildingSearchRequest {
+    private String name ;
+    private Long floorArea;
+    private Long districtId;
+    private String ward;
+    private String street;
+    private String direction;
+    private String level;
+    private Long numberOfBasement;
+    private Long areaRentStart;
+    private Long areaRentFinish;
+    private Long priceRentStart;
+    private Long priceRentFinish;
+    private String managerName;
+    private String managerPhoneNumber;
+    private Long staffId;
+    private List<String> code;
 }
